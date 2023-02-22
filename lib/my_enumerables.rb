3bb8fbb81055484a8c0c6e08a3f8136e8db9939f
Jacob Sanders <sanders.jms@gmail.com>
2023-02-22 14:21:13 -0700

    finished my all and my any methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -30,6 +30,15 @@ module Enumerable
     array_to_return
   end
 
+  def my_select
+    return to_enum unless block_given?
+    array_to_return = []
+    for elem in self
+      array_to_return << elem if yield(elem)
+    end
+    array_to_return
+  end
+
   def my_count(arg=nil)
     count = 0
     for elem in self
@@ -38,13 +47,37 @@ module Enumerable
           count += 1
         end
       elsif block_given?
-        count += 1 if yield(elem) == true
+        count += 1 if yield(elem)
       else
         count += 1
       end
     end
     count
   end
+
+  def my_all?(pattern=nil)
+    self.my_each do |elem|
+      if pattern
+        return false unless pattern == elem
+      elsif block_given?
+        return false unless yield elem
+      else
+        return false unless elem
+      end
+    end
+    true
+    # block = block_given? ? ->(elem) { yield elem } : ->(elem) { pattern === elem } 
+    # self.my_each { |elem| return false unless block.call(elem) }
+    # true
+  end
+
+  def my_any?(pattern=nil)
+    block_given? ? block = ->(elem) { yield elem } : block = ->(elem) { elem }
+    block = ->(elem) { pattern === elem } if pattern
+    self.my_each { |elem| return true if block.call(elem) }
+    false 
+  end
+
 end
 
 
